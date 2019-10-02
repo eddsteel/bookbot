4176791c9be71304039b81cd66159a6237c236c0
Edd Steel <edd@eddsteel.com>
2019-10-01 19:57:48 -0700

    Make spider script quieter

--- scripts/spider-notebook.sh
+++ scripts/spider-notebook.sh
@@ -9,10 +9,11 @@
 mkdir -p $BOOK_DIRECTORY
 mkdir -p tmp
 
-wget -O tmp/notebook --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' --header 'Accept: */*' --header 'Accept-Language: en-US,en;q=0.5' --header 'DNT: 1' --header 'Connection: keep-alive' --header "Cookie: $COOKIE" https://read.amazon.com/notebook
+wget -q -O tmp/notebook --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' --header 'Accept: */*' --header 'Accept-Language: en-US,en;q=0.5' --header 'DNT: 1' --header 'Connection: keep-alive' --header "Cookie: $COOKIE" https://read.amazon.com/notebook
 
 grep -Eo 'asin&quot;:&quot;.{10}&quot;' tmp/notebook | cut -c18-27 | while read asin; do
-    wget -O "$BOOK_DIRECTORY/$asin" \
+    echo $asin
+    wget -q -O "$BOOK_DIRECTORY/$asin" \
          --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' \
          --header 'Accept: */*' \
          --header 'Accept-Language: en-US,en;q=0.5' \
